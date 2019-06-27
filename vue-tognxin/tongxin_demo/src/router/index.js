fe7bdcfee476d2fa95f9a7b885a999762de7ef62
lijianjie123 <505604254@qq.com>
2019-06-27 19:19:37 +0800

    组件

--- vue-tognxin/tongxin_demo/src/router/index.js
+++ vue-tognxin/tongxin_demo/src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import form from "@/components/form/form"  //引入
+import index from "@/components/form/index"  //引入
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'form',
-      component: form
+      name: 'index',
+      component: index
     }
   ]
 })
