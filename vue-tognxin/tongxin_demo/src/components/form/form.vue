9f3ce5902457faa4ff9394fab5cfea82bd01521f
lijianjie123 <505604254@qq.com>
2019-06-27 18:58:06 +0800

    组件实践

---
+++ vue-tognxin/tongxin_demo/src/components/form/form.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <slot></slot>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Input',
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ vue-tognxin/tongxin_demo/src/components/form/formitem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <label></label>
+    <p></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FormItem',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
